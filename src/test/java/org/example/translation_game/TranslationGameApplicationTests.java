9accfac41c212fbe383b67f155888e8df802d443
newnewminsuk <rhalstjr106@gmail.com>
2024-11-27 02:55:06 +0900

    첫번째 기틀.

---
+++ src/test/java/org/example/translation_game/TranslationGameApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.translation_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslationGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
